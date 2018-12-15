850ceaeb0b9847ebd45487f67a6d1406691f4698
Atul <atulasati@gmail.com>
2018-12-15 13:03:23 +0530

    refactor

--- Project_CPP/GuessingGame.cpp
+++ Project_CPP/GuessingGame.cpp
@@ -1,68 +1,91 @@
 #include<iostream>
 #include<stdlib.h>
 #include<time.h>
-#include <string> 
-#include <sstream> 
+#include <string>
+#include <sstream>
 
 using namespace std;
 
-bool isNumber(const std::string& s)
+bool isNumber(const string& s)
 {
-    std::string::const_iterator it = s.begin();
+    string::const_iterator it = s.begin();
     while (it != s.end() && std::isdigit(*it)) ++it;
     return !s.empty() && it == s.end();
 }
 
-void matchGuessNoWithRandomNumber()
+bool checkInput(string& strGuessedNumber)
+{
+    getline (cin, strGuessedNumber);
+    if (isNumber(strGuessedNumber) == 0){
+        cout << "Invalid entry!" << endl;
+        return false;
+    }
+
+    return true;
+}
+
+int generateRandNo()
+{
+    int generatedNo;
+    generatedNo = rand()%100 + 1;
+    return generatedNo;
+}
+
+bool matchGuessNoWithRandomNumber(int generatedNumber)
 {
     string strGuessedNumber;
-    int guessedNumber, generatedNumber, noOfTries;
-    cout << "I chose a number between 1 and 100" << endl;
-    cout << "Can you guess that number: " << endl;
-    generatedNumber = rand()%100 + 1;
+    int guessedNumber, noOfTries;
     noOfTries = 0;
 
-    do
+    if(!checkInput(strGuessedNumber))
     {
-        noOfTries++;
-        getline (cin, strGuessedNumber); 
-        if (isNumber(strGuessedNumber) == 0){
-            cout<<"Invalid entry! Guess again: ";
-            continue;
-        }
-        stringstream strToNum(strGuessedNumber);
-        strToNum >> guessedNumber;
-        cout << endl;
+        return false;
+    }
 
-        if (guessedNumber < generatedNumber)
-        {
-            cout << "Too low, the number I have chosen is greater than " << guessedNumber << endl;
-        }
-        else if(guessedNumber > generatedNumber)
-        {
-            cout << "Too high, the number I chose is smaller than " << guessedNumber << endl;
-        }
-        cout << "To try a NEW: "; 
-    }while(guessedNumber != generatedNumber);
+    stringstream strToNum(strGuessedNumber);
+    strToNum >> guessedNumber;
 
-    cout << "==================================" << endl;
-    cout << "Bravo! You have Guess The number after " << noOfTries << " tries" << endl;
+    if (guessedNumber < generatedNumber)
+    {
+        cout << "Too low, the number I have chosen is greater than " << guessedNumber << endl;
+        return false;
+    }
+    else if(guessedNumber > generatedNumber)
+    {
+        cout << "Too high, the number I chose is smaller than " << guessedNumber << endl;
+        return false;
+    }
+
+    return true;
 }
- 
+
 
 int main()
 {
-    // This program will create different sequence of  
-    // random numbers on every program run  
+    // This program will create different sequence of
+    // random numbers on every program run
 
-    // Use current time as seed for random generator 
+    // Use current time as seed for random generator
     srand(time(0));
 
     cout << "Game-Guess my number Version 1.0" << endl;
     cout << "==================================" << endl;
 
-    matchGuessNoWithRandomNumber();
-    getchar();
+    cout << "I chose a number between 1 and 100" << endl;
+    cout << "Can you guess that number: ";
+
+    int noOfTries;
+    int generatedNumber = generateRandNo();;
+    noOfTries = 0;
+
+    while(!matchGuessNoWithRandomNumber(generatedNumber))
+    {
+        noOfTries++;
+        cout << "To try a NEW: ";
+    }
+
+    cout << "==================================" << endl;
+    cout << "Bravo! You have Guess The number after " << noOfTries << " tries" << endl;
 
     return 0;
 }

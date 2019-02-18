2f09f2da7ce78dada2a19cb29af6eb4c83839963
Atul <atulasati@gmail.com>
2019-02-18 21:08:16 +0530

    author done

--- author/python_VotreNom.py
+++ author/python_VotreNom.py
@@ -22,111 +22,95 @@
 
 import os
 import re
-import xml
+import errno
+
+from xml.etree.ElementTree import Element, SubElement, tostring
+import xml.etree.cElementTree as ET
+
+def create_file(file_path):
+    if not os.path.exists(os.path.dirname(file_path)):
+        try:
+            os.makedirs(os.path.dirname(file_path))
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise
 
 
-def validate_cuorse_name(course_name):
-    return len(course_name) > 5
+def validate_title(title):
+    return True if re.search("^[A-Za-z0-9\\s\\-,]+$", title) else False
 
-class Course(object):
-    def __init__(self, name):
-        self.name = name
-        self.students_list = []
+def validate_author(author):
+    return True if re.search("^[A-Za-z0-9\\s\\-,]+$", author) else False
 
-    def add_students(self, student):
-        self.students_list.append(student)
+def validate_year(year):
+    if year and year.isdigit():
+        if int(year) >=1900 and int(year) <=2018:
+            return True
+    return False
 
-    def return_course_average(self):
-        return sum([float(student.grade) for student in self.students_list])/len(self.students_list)        
+def validate_price(price):
+    try:
+        if len(price.split('.')) !=2 or (not price.split('.')[1]): 
+          return False
+        price = float(price)
+    except:
+        return False
+    return True
 
-    def return_student_average(self, student_name):
-        for student in self.students_list:
-            if student.name == student_name.strip():
-                return student
-        return None
+print ("Devoir 1 par Katy Mallard")
+print ("Welcome to my library!\n")
 
-    def return_all_students(self):
-        return self.students_list
+your_name = "Katy Malland"
+root = Element('library')
+root.set("name", your_name)
 
-    def __str__(self):
-        return "<Course:%s - %s>" %(self.name, len(self.students_list))
+id = 0
+while True:
+    while (True):
+        book_title = input ("What is the title of the book? ")
+        if validate_title(book_title):
+            break
+        print ("Invalid title. ", end="")
 
-print ("Devoir 3 par Anthony Ilunga")
-print ("Utilisation de classe et module. Script pour calculer la moyenne d'un cours.\n")
+    while (True):
+        author_name = input ("Who is the author? ")
+        if validate_author(author_name):
+            break
+        print ("Invalid author. ", end="")
 
-while (True):
-    course_name = input ("Quel est le nom du cours? ")
-    if validate_cuorse_name(course_name):
-        course = Course(name=course_name)
+    while (True):
+        pub_year = input ("What year was it published? ")
+        if validate_year(pub_year):
+            break
+        print ("Invalid year. ", end="")
+
+    while (True):
+        book_price = input ("What is the price of the book? ")
+        if validate_price(book_price):
+            break
+        print ("Invalid price. ", end="")
+
+    id += 1
+
+    book = SubElement(root, "book")
+    book.set("id", str(id))
+    title = SubElement(book, "title")
+    title.text = book_title
+    author = SubElement(book, "author")
+    author.text = author_name
+    year = SubElement(book, "year")
+    year.text = pub_year
+    price = SubElement(book, "price")
+    price.text = book_price
+
+    another_book = input ("Enter another book? (y or Y): ") 
+    if another_book.strip().lower() != 'y':
+        tree = ET.ElementTree(root)
+        file_path = "%s/mylibrary.xml" %your_name
+        create_file(file_path)
+        tree.write(file_path)
+
+
+        print("File Saved!\n")
+        input("Pres Enter key to exit.")
         break
-    print ("Nom invalide. ", end="")
-
-while(True):
-    order = "\n-------------------------------------\n"
-    order += "1 - Inscrire des étudiants au cours\n"
-    order += "2 - Afficher la moyenne du cours\n"
-    order += "3 - Afficher la moyenne d'un étudiant(e)\n"
-    order += "4 - Afficher tous les étudiant(e)s\n"
-    order += "5 - Recommencez pour un nouveau cours\n"
-    order += "6 - Sortir\n"
-    order += "Veuillez choisir une commande: "
-    order = input (order)
-
-    if order == "1":
-        while (True):
-            while (True):
-                student_name = input ("Quel est le nom de l'étudiant(e)? ")
-                if PROG1326_Student_VotreNom.validate_student_name(student_name):
-                    break
-                print ("Nom invalide. ", end="")
-
-            while (True):
-                student_grade = input ("Quelle est sa moyenne? ")
-                if PROG1326_Student_VotreNom.validate_student_grade(student_grade):
-                    break
-            student = PROG1326_Student_VotreNom.Student(student_name, student_grade)
-            course.add_students(student)
-            add_more_student = input ("Voulez-vous entrer un autre étudiant(e)? (o ou n) ")
-            if add_more_student != 'o':
-                break
-
-    elif order == "2":
-        if course.return_all_students():
-            print("La moyenne du cours %s est de : %s" %(course.name, course.return_course_average()))
-        else:
-            print ("Il n’y a pas d’étudiant(e) dans le cours!")
-
-    elif order == "3":
-        if course.return_all_students():
-            student_name = input ("Entez le nom de l'étudiant(e): ")
-            student = course.return_student_average(student_name)
-            if student:
-                print ("La moyenne de cet étudiant(e) est de : %s" %student.grade)
-            else:
-                print ("L'étudiant(e) n'est pas inscrit à ce cours.") 
-        else:
-            print ("Il n’y a pas d’étudiant(e) dans le cours!")
-
-    elif order == "4":
-        students = course.return_all_students()
-        if students:
-            print ("Voici la liste d'étudiant(e)s du cours %s " %course.name)
-            for student in students:
-                print ("%s %.1f" %(student.name, student.grade))
-        else:
-            print ("Il n’y a pas d’étudiant(e) dans le cours!")
-    elif order == "5":
-        while (True):
-            course_name = input ("Quel est le nom du cours? ")
-            if validate_cuorse_name(course_name):
-                course = Course(name=course_name)
-                break
-            print ("Nom invalide.", end="")
-
-    elif order == "6":
-            break   
-    else:
-        print("Commande Invalide.")
-
-input("Press Enter key to exit.")
-
